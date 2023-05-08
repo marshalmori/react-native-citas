d04b4526a109a284f1a185c58108307b17e95fe3
Marshal Mori Cavalheiro <marshal@laura.asimov>
2023-05-07 23:00:23 -0300

    Aplicar styles com StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>Hola Mundo</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>
+        Administrador de Citas {''}
+        <Text style={styles.tituloBold}>Veterinaria</Text>
+      </Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1,
+  },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600',
+  },
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6D28D9',
+  },
+});
 
 export default App;
