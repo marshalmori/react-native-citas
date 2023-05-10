39b117d8837f5f50e8f1e7002de690b23fa9723f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-10 09:39:57 -0300

    Criar os estilos para o componente paciente

--- src/components/Paciente.js
+++ src/components/Paciente.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const Paciente = ({item}) => {
   const {paciente, fecha} = item;
@@ -17,11 +17,34 @@ const Paciente = ({item}) => {
   };
 
   return (
-    <View>
-      <Text>{paciente}</Text>
-      <Text>{formatearFecha(fecha)}</Text>
+    <View style={styles.contenedor}>
+      <Text style={styles.label}>Paciente:</Text>
+      <Text style={styles.texto}>{paciente}</Text>
+      <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#FFF',
+    padding: 20,
+    borderBottomColor: '#94A3B8',
+    borderBottomWidth: 1,
+  },
+  label: {
+    color: '#374151',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  texto: {
+    color: '#6D28D9',
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  fecha: {},
+});
 
 export default Paciente;
